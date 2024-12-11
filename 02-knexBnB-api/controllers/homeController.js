9996092550033a990503d9ed6fa730754cbb1319
César Guerra <warderer@gmail.com>
2024-12-10 20:02:16 -0600

    ✨ Add Controller with createHome

---
+++ 02-knexBnB-api/controllers/homeController.js
@@ -0,0 +1,28 @@
+/* CONTROLADORES */
+// Los controladores tienen la lógica de negocios de la aplicación.
+// Se encargar de recibir las peticiones HTTP, procesarlas y enviar una respuesta adecuada.
+// También pueden validar los datos que se envian, manejar errores y resolver promesas.
+
+// #1 Importar el modelo, en este caso Home.js
+const ModelHome = require('../models/Home')
+
+// #2 Crear funciones que se van a ejecutar cuando se haga una petición HTTP y que serán llamadas a través de las rutas (vistas)
+
+// CREATE
+const createHome = (req, res) => {
+  ModelHome.create(req.body).then(home => {
+    res.status(201).json(home)
+  }).catch(error => {
+    res.status(400).json({ error: error.message })
+  })
+}
+
+// READ
+
+// UPDATE
+
+// DELETE
+
+module.exports = {
+  createHome
+}
